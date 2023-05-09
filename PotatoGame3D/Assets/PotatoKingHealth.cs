33c26b20229db62f210d6581bb657d2a848d2b4f
Asuran Delarosa <115186200+Asupapas@users.noreply.github.com>
2023-05-09 13:22:23 -0700

    created potatoking health script

---
+++ PotatoGame3D/Assets/PotatoKingHealth.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PotatoKingHealth : MonoBehaviour
+{
+    public int maxHp = 10;
+    public int currentHp;
+
+    private void Start()
+    {
+        currentHp = maxHp;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        if (currentHp <= 0)
+        {
+            Defeated();
+        }
+    }
+
+    private void Defeated()
+    {
+        SceneManager.LoadScene("Ending");
+    }
+
+    internal void TakeDamage(float damage)
+    {
+        throw new NotImplementedException();
+    }
+}
